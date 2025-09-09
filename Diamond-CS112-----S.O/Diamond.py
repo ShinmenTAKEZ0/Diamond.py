208c799a20966ad1c72c6bb7dffc5a3bf55c0ab6
67 <steorn1222@azusa.org>
2025-09-09 11:26:38 -0700

    Add files via upload

--- Diamond-CS112-----S.O/Diamond.py
+++ Diamond-CS112-----S.O/Diamond.py
@@ -3,5 +3,16 @@ This was created with my partner Mr.Andres
 September 8th, 2025
 """
 
-
+#required number variables
+counter,rowsDrawn,spacesOutside,spacesInside,starsToDraw,userInput = 0,0,0,0,0,0
+#String variables required
+output = ""
 num1 = int(input("Enter an odd number from (1-13):"))
+
+#This is going to do my top part
+#I'm using a method/function to organize
+def top_diamond(num1):
+    topString = "*"
+    return topString
+output = top_diamond(num1)
+print(output)
